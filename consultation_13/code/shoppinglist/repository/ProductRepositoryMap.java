489bbe3ebb15b00ca1e5edab7aad4a5fdeac382c
Valerii Kuchynskyi <valeryk@hotmail.com>
2024-06-15 18:16:35 +0200

    add

---
+++ consultation_13/code/shoppinglist/repository/ProductRepositoryMap.java
@@ -0,0 +1,37 @@
+package code.shoppinglist.repository;
+
+/*
+Возможный вариант альтернативного решения для репозитория
+изменения, которые НИКАК не влияют на работу сервиса
+ */
+//public class ProductRepositoryMap implements ProductRepository{
+//
+//    private Map<Integer,Product> database;
+//
+//    private Integer idCounter;
+//
+//    public ProductRepositoryMap() {
+//        this.database = new HashMap<>();
+//        this.idCounter = 0;
+//    }
+//
+//    @Override
+//    public Integer addProduct(ProductDto productDto) {
+//        return 0;
+//    }
+//
+//    @Override
+//    public List<Product> findAll() {
+//        return List.of();
+//    }
+//
+//    @Override
+//    public Optional<Product> findById(Integer id) {
+//        return Optional.empty();
+//    }
+//
+//    @Override
+//    public List<Product> findByProductName(String productName) {
+//        return new ArrayList<>();
+//    }
+//}
